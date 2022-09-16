8d1ce6b35ca5d0e58cfd5680452bb0782a14faca
David Esteban Suarez Rodriguez <kalevenus.net@gmail.com>
2022-09-16 19:31:12 +0200

    Impacto

---
+++ PR01-DEIM/Assets/Scripts/NuevoMetodo.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuevoMetodo : MonoBehaviour
+
+
+{
+    [SerializeField] float shield = 100;
+    [SerializeField] float hp = 100;
+    [SerializeField] bool alive = true;
+    [SerializeField] int ammo = 200;
+    [SerializeField] int lifes = 3;
+    [SerializeField] string message = "holi";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifes = 3;
+        shield = 100f;
+        Impacto();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Han pulsado el espacio");
+            if(alive)
+             Impacto();
+
+
+        }
+    }
+
+    //Nuevo mtodo
+
+    void Impacto()
+    {
+    
+        hp = Random.Range(1f, 100f);
+        shield -= hp;
+
+        if(shield < 0)
+        {
+
+            lifes--;
+            if (lifes == 0) 
+            {
+                message = "has morido";
+                alive = false;
+
+            }
+            else
+            {
+                shield = 100f;
+                message = "Una vida menos crack, te quedan" + lifes;
+                Invoke("impacto", 10f);
+
+            }
+         
+         
+        }
+        else
+        {
+            shield = 100f;
+            message = "te han impactado" + hp + "te queda " + shield + "de vida";
+            Invoke("impacto", 10f);
+
+        }
+
+        message = "me han dao" + hp;
+
+        print(message);
+
+
+      
+
+    }
+
+}
+
